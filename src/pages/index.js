853bde096c44ccec8576664056c7eb5e5023c342
Simon Neaves <simon@aerian.com>
2022-11-06 22:12:11 +0000

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -93,7 +93,7 @@ const IndexPage = () => (
           </React.Fragment>
         ))}
         <br />
-        Edit <code>src/pages/index.js</code> to update this page.
+        Edit <code>src/pages/index.js</code> to update this page. ✅
       </p>
     </div>
     <ul className={styles.list}>
